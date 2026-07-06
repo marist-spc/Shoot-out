560486c46cc5bd47e85a09af1ad1bf755f6b19b9
Jace R <jacereyna.ga@gmail.com>
2026-07-06 09:23:55 -0400

    Created local multiplayer movement
    {Title}
    
    The players can either be controller + controller or keyboard + controller
    
    Blue cyllinders around the map are

---
+++ shoot-out/main.gd
@@ -0,0 +1,83 @@
+extends Node2D
+
+signal player1IsKeyboard
+var isPlayer1Keyboard := true
+
+func initialize():
+	for i in range(1,3):
+		if isPlayer1Keyboard and i == 1:
+			create_input("up",i, KEY_W)
+			create_input("left",i, KEY_A)
+			create_input("down",i,KEY_S)
+			create_input("right",i, KEY_D)
+		else:
+			create_input("up",i, KEY_0, Vector2(0,-1))
+			create_input("left",i, KEY_0, Vector2(-1,0))
+			create_input("down",i,KEY_0, Vector2(0,1))
+			create_input("right",i, KEY_0, Vector2(1,0))
+			create_input("shoot",i,KEY_0, Vector2.ZERO, JOY_BUTTON_RIGHT_SHOULDER)
+			create_input("aim_up",i, KEY_0, Vector2(0,-1), JOY_BUTTON_A, true)
+			create_input("aim_left",i, KEY_0, Vector2(-1,0), JOY_BUTTON_A, true)
+			create_input("aim_down",i,KEY_0, Vector2(0,1), JOY_BUTTON_A, true)
+			create_input("aim_right",i, KEY_0, Vector2(1,0), JOY_BUTTON_A, true)
+
+			
+		
+	
+func create_input(
+Inputname : String,
+player : int,
+key : Key, 
+joyPadDirection := Vector2.ZERO,
+joy_pad_button := JoyButton.JOY_BUTTON_A,
+isRightAxis := false):
+	var action_name = Inputname + str(player)
+	InputMap.add_action(action_name)
+	if  player == 1 and isPlayer1Keyboard :
+		var input_key = InputEventKey.new() 
+		input_key.keycode = key
+		InputMap.action_add_event(action_name,input_key)
+	else:
+		if joyPadDirection != Vector2.ZERO:
+			var event = InputEventJoypadMotion.new()
+			var isHorizontal = joyPadDirection.x != 0
+			var dir := joyPadDirection.x if isHorizontal else joyPadDirection.y
+			if	!isRightAxis:
+				event.axis = JOY_AXIS_LEFT_X if isHorizontal else JOY_AXIS_LEFT_Y
+			else:
+				event.axis = JOY_AXIS_RIGHT_X if isHorizontal else JOY_AXIS_RIGHT_Y
+
+			event.axis_value = dir
+			if isPlayer1Keyboard:
+				event.device = 0
+			else:
+				event.device = player - 1
+			InputMap.action_add_event(action_name, event)
+		else:
+			var input_key = InputEventJoypadButton.new() 
+			input_key.button_index  = joy_pad_button
+			if isPlayer1Keyboard:
+				input_key.device = 0
+			else:
+				input_key.device = player - 1
+			InputMap.action_add_event(action_name,input_key)
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var controllers := Input.get_connected_joypads()
+	if controllers.size() >= 2:
+		isPlayer1Keyboard = false
+		print("2 CONTROLLERS FOUND")
+	elif controllers.size() == 1:
+		isPlayer1Keyboard = true
+		player1IsKeyboard.emit()
+		print("1 CONTROLLER FOUND")
+	else:
+		print("NO CONTROLLER FOUND")
+	initialize()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
